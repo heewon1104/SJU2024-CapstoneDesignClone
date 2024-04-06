4e2197caadfaabf2a2b41f42ac675f8a42d6bbef
Smith <125195487+sigmaith@users.noreply.github.com>
2024-04-06 20:40:40 +0900

    [IN-54] 로그인 기능 구현완료, jwt 발행 확인.

---
+++ backend/src/main/java/org/example/capstonenewri/Service/LoginService.java
@@ -0,0 +1,7 @@
+package org.example.capstonenewri.Service;
+
+import org.example.capstonenewri.Dto.RequestLoginDto;
+
+public interface LoginService {
+    String login(RequestLoginDto requestLoginDto);
+}
