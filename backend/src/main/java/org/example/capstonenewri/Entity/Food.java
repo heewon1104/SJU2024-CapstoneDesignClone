5af2f36715a6f734e73d82caec98ce735565388d
Smith <125195487+sigmaith@users.noreply.github.com>
2024-04-17 17:49:55 +0900

    Food-Nutrition Dataset embedding

---
+++ backend/src/main/java/org/example/capstonenewri/Entity/Food.java
@@ -0,0 +1,48 @@
+package org.example.capstonenewri.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder @Getter
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "food_code")
+    private String food_code;
+    private String food_name;
+    private String amount;
+
+    private BigDecimal energy_kcal;
+    private BigDecimal water_gram;
+    private BigDecimal protein_gram;
+    private BigDecimal fat_gram;
+    private BigDecimal ashcontent_gram;
+    private BigDecimal carbohydrate_gram;
+    private BigDecimal sugars_gram;
+    private BigDecimal dietary_fiber_gram;
+    private BigDecimal calcium_miligram;
+    private BigDecimal iron_miligram;
+    private BigDecimal phosphorus_miligram;
+    private BigDecimal potassium_miligram;
+    private BigDecimal sodium_miligram;
+    private BigDecimal vitaminA_microgram;
+    private BigDecimal retinol_microgram;
+    private BigDecimal betaCarotene_microgram;
+    private BigDecimal thiamin_miligram;
+    private BigDecimal riboflavin_miligram;
+    private BigDecimal niacin_miligram;
+    private BigDecimal vitaminC_miligram;
+    private BigDecimal vitaminD_microgram;
+    private BigDecimal cholesterol_miligram;
+    private BigDecimal saturated_fatty_acids_gram;
+    private BigDecimal trans_fatty_acids_gram;
+
+    private String source;
+    private String single_intake;
+
+}
