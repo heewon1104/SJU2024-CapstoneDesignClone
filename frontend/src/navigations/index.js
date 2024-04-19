fd67575b4861fc53df9e85797a200ef22f08115d
김희원 <julian3306@naver.com>
2024-04-19 17:03:13 +0900

    [IN-53] Token 주소 Local Storage에 저장

--- frontend/src/navigations/index.js
+++ frontend/src/navigations/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Auth from './Auth';
 import Main from './Main';
 import { UserLoginInfoContext } from '../contexts';
-import { useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text } from 'react-native';
 
 const Navigations = () => {
   const tokenInfo = useContext(UserLoginInfoContext);
+  const [value, setValue] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log('loading22');
+      try {
+        const storedValue = await AsyncStorage.getItem('TA223344');
+        console.log(storedValue);
+        setValue(storedValue);
+        console.log(tokenInfo.userTokenCheck);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <NavigationContainer>
-      {tokenInfo.userToken == null ? <Auth></Auth> : <Main></Main>}
+      {tokenInfo.userTokenCheck === true || value != null ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 };
+
 export default Navigations;
