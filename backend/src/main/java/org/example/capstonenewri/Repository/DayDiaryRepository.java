c34ea278d6f01f7fdbf7dc8471eecdaeb771629b
Smith <125195487+sigmaith@users.noreply.github.com>
2024-05-23 11:30:24 +0900

    [IN-95] api/mypage/{date}
    MyPageSevenDayDto.java : 데이터 정의
    MyPageController.java : 메인페이지에서 7일간의 데이터 요청 처리
    MyPageService.java
    MyPageServiceImpl.java : 멤버 객체 찾기, 7일 단위 맞추기(LocalDate)
    DayDiaryRepository.java : 각 7일단위의 날짜 해당 daydiary 에서 탄,단,지,칼로리,날짜 데이터 뽑아 Dto배열로 뽑는 쿼리

--- backend/src/main/java/org/example/capstonenewri/Repository/DayDiaryRepository.java
+++ backend/src/main/java/org/example/capstonenewri/Repository/DayDiaryRepository.java
@@ -1,5 +1,6 @@
 package org.example.capstonenewri.Repository;
 
+import org.example.capstonenewri.Dto.MyPageSevenDayDto;
 import org.example.capstonenewri.Entity.DayDiary;
 import org.example.capstonenewri.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +18,14 @@ public interface DayDiaryRepository extends JpaRepository<DayDiary, Long> {
 
     @Query(" select d.feedback from DayDiary d where d.member = :member and d.date = :date ")
     Optional<String> findDailyFeedbackByMemberAndDate(@Param("member") Member member, @Param("date") LocalDate date);
+
+    @Query("""
+        select new org.example.capstonenewri.Dto.MyPageSevenDayDto(d.carbohydrate_gram, d.protein_gram, d.fat_gram, d.energy_kcal, d.date)
+        from DayDiary d
+        where d.member = :member and d.date between :startDate and :endDate
+    """)
+    Optional<List<MyPageSevenDayDto>> findSevenDtoByMemberAndPeriod(
+            @Param("member") Member member,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
 }
