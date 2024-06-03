afa5a70a575f0222e054ce466deba055020c7c18
Smith <125195487+sigmaith@users.noreply.github.com>
2024-06-03 19:42:56 +0900

    [IN-85] api/mypage/instruction -> api/recipe/instruction 이동

--- backend/src/main/java/org/example/capstonenewri/Controller/RecipeController.java
+++ backend/src/main/java/org/example/capstonenewri/Controller/RecipeController.java
@@ -2,6 +2,7 @@ package org.example.capstonenewri.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.capstonenewri.Dto.ResponseRecipeDto;
+import org.example.capstonenewri.Service.MyPageServiceImpl;
 import org.example.capstonenewri.Service.RecipeServiceImpl;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ResponseStatus(HttpStatus.OK)
 @RestController
@@ -18,9 +21,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RecipeController {
     private final RecipeServiceImpl recipeServiceImpl;
+    private final MyPageServiceImpl myPageServiceImpl;
 
     @GetMapping("/{date}")
     public ResponseEntity<List<ResponseRecipeDto>> getTodayRecipes(Authentication authentication, @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date){
         return recipeServiceImpl.findRecipeByDate(authentication.getName(), date);
+    }
+
+    @GetMapping("/instruction")
+    public Map<String, Object> getInstruction(Authentication authentication){
+        String instruction = myPageServiceImpl.getInstructionByMemberEmail(authentication.getName());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("instruction", instruction);
+
+        return response;
     }
 }
